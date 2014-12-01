c06ca705220388dac94bc441954e81c007ab8b0f
Yaron Rosenbaum <yaron.rosenbaum@gmail.com>
2014-12-01 13:10:37 +0200

    Fail if something goes wrong, verify env var

--- haproxy-confd/entrypoint.sh
+++ haproxy-confd/entrypoint.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+if [ -z "$ETCD_NODE" ]
+then
+  echo "Missing ETCD_NODE env var"
+  exit -1
+fi
+
+set -eo pipefail
+
 #confd will start haproxy, since conf will be different than existing (which is null)
 
 echo "[haproxy-confd] booting container. ETCD: $ETCD_NODE"
 
 # Loop until confd has updated the haproxy config
+
 until confd -onetime -node $ETCD_NODE; do
   echo "[haproxy-confd] waiting for confd to refresh haproxy.cfg"
   sleep 5
