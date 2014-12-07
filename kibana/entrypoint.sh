30fc222d47cfe63ab1e778d1558e5cb5b7230b1a
Yaron Rosenbaum <yaron.rosenbaum@gmail.com>
2014-12-07 13:17:40 +0200

    initial commit, kibana

---
+++ kibana/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo pipefail
+
+sed "s/^elasticsearch:.*/elasticsearch: http:\/\/$1:$2/" -i /opt/kibana/config/kibana.yml
+
+echo "Taking a nap, to allow weave network to properly set up.."
+sleep 10
+
+echo "That was a good nap. Now to work..."
+/opt/kibana/bin/kibana "$@"
