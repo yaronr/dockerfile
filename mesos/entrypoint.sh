12d307227ace3c6cfc74c76cb736f10552c00f38
Yaron Rosenbaum <yaron.rosenbaum@gmail.com>
2015-01-28 12:36:16 +0200

    properly handle multiline

--- mesos/entrypoint.sh
+++ mesos/entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+if [[ -n $MULTICLOUD_DNS && -n MULTICLOUD_DNS_SEARCH ]] ; then
+  echo "Replacing /etc/resolv.conf with:"
+  echo "$MULTICLOUD_DNS" 
+  echo "$MULTICLOUD_DNS_SEARCH"
+  echo "$MULTICLOUD_DNS" > /etc/resolv.conf
+  echo "$MULTICLOUD_DNS_SEARCH" >> /etc/resolv.conf
+fi;
+
 echo "Taking a nap, to allow weave network to properly set up.."
 sleep 10
-
-if [[ -n $MULTICLOUD_RESOLV_CONF ]] ; then
-  echo "Replacing /etc/resolv.conf with:"
-  echo "$MULTICLOUD_RESOLV_CONF" 
-  echo "$MULTICLOUD_RESOLV_CONF" > /etc/resolv.conf
-fi;
 
 echo "That was a good nap. Now to work..."
 exec "$@"
