37aa328b11d424ba86c3a326f66d3e990b48ce9c
Yaron Rosenbaum <yaron.rosenbaum@gmail.com>
2015-01-28 13:13:44 +0200

    type

--- mesos/entrypoint.sh
+++ mesos/entrypoint.sh
@@ -2,10 +2,10 @@
 
 if [[ -n $MULTICLOUD_DNS && -n MULTICLOUD_DNS_SEARCH ]] ; then
   echo "Replacing /etc/resolv.conf with:"
-  echo "$MULTICLOUD_DNS" 
-  echo "$MULTICLOUD_DNS_SEARCH"
-  echo "$MULTICLOUD_DNS" > /etc/resolv.conf
-  echo "$MULTICLOUD_DNS_SEARCH" >> /etc/resolv.conf
+  echo "nameserver $MULTICLOUD_DNS" 
+  echo "search $MULTICLOUD_DNS_SEARCH"
+  echo "nameserver $MULTICLOUD_DNS" > /etc/resolv.conf
+  echo "search $MULTICLOUD_DNS_SEARCH" >> /etc/resolv.conf
 fi;
 
 echo "Taking a nap, to allow weave network to properly set up.."
