99d96abe2be20ef2397040dd87e242c004fbef03
Yaron Rosenbaum <yaron.rosenbaum@gmail.com>
2015-01-28 11:09:01 +0200

    Replace resolv.conf with content of/ if env var MULTICLOUD_RESOLV_CONF is set

--- mesos/entrypoint.sh
+++ mesos/entrypoint.sh
@@ -3,5 +3,11 @@
 echo "Taking a nap, to allow weave network to properly set up.."
 sleep 10
 
+if [[ -n $MULTICLOUD_RESOLV_CONF ]] ; then
+  echo "Replacing /etc/resolv.conf with:"
+  echo "$MULTICLOUD_RESOLV_CONF" 
+  echo "$MULTICLOUD_RESOLV_CONF" > /etc/resolv.conf
+fi;
+
 echo "That was a good nap. Now to work..."
 exec "$@"
