a5bb74311adb8a90ce151e221beaa26f989b7078
Yaron Rosenbaum <yaron.rosenbaum@gmail.com>
2015-07-14 15:40:18 +0300

    remove sleep. change elasticsearch: to elasticsearch_url:"

--- kibana/entrypoint.sh
+++ kibana/entrypoint.sh
@@ -2,10 +2,6 @@
 
 set -eo pipefail
 
-sed "s/^elasticsearch:.*/elasticsearch: http:\/\/${elasticsearch_server_url}:${elasticsearch_server_port}/" -i /opt/kibana/config/kibana.yml
+sed "s/^elasticsearch_url:.*/elasticsearch_url: http:\/\/${elasticsearch_server_url}:${elasticsearch_server_port}/" -i /opt/kibana/config/kibana.yml
 
-echo "Taking a nap, to allow weave network to properly set up.."
-sleep 10
-
-echo "That was a good nap. Now to work..."
 /opt/kibana/bin/kibana
