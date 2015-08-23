899c5476e435b7c284b6b2b5738fa761da469f92
Yaron Rosenbaum <yaron.rosenbaum@gmail.com>
2015-08-23 10:12:33 +0300

    remove sleep. zookeeper: set my_id from env var

--- mesos/entrypoint.sh
+++ mesos/entrypoint.sh
@@ -8,8 +8,5 @@ if [[ -n $MULTICLOUD_DNS && -n MULTICLOUD_DNS_SEARCH ]] ; then
   echo "search $MULTICLOUD_DNS_SEARCH" >> /etc/resolv.conf
 fi;
 
-echo "Taking a nap, to allow weave network to properly set up.."
-sleep 10
-
 echo "That was a good nap. Now to work..."
 exec "$@"
--- zookeeper/entrypoint.sh
+++ zookeeper/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-echo "Taking a nap, to allow weave network to properly set up.."
-sleep 10
+if [[ -n $ZOOKEEPER_ID ]] ; then
+  echo "Setting zookeeper ID to: $ZOOKEEPER_ID"
+  echo "$ZOOKEEPER_ID" > /tmp/zookeeper/myid
+else
+  echo "ZOOKEEPER_ID must be specified"
+  exit 1
+fi
 
-echo "That was a good nap. Now to work..."
 exec "$@"
