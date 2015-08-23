a91cdebfa4bb6817d1134903c173c7efe8fa3f03
Yaron Rosenbaum <yaron.rosenbaum@gmail.com>
2015-08-23 22:03:11 +0300

    revert to 3.4.6, re-add sleep 30s

--- zookeeper/entrypoint.sh
+++ zookeeper/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "taking a nap, to allow other zookeeper instances to start and update in DNS"
+sleep 30
+
 if [[ -n $ZOOKEEPER_ID ]] ; then
   echo "Setting zookeeper ID to: $ZOOKEEPER_ID"
   echo "$ZOOKEEPER_ID" > /tmp/zookeeper/myid
